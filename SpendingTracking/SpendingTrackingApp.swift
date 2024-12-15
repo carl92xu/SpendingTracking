1d30b164f13a580bc1462465091614c792459f0b
Carl Xu <carl92xu@gmail.com>
2024-12-14 21:28:44 -0600

    Initial Commit

---
+++ SpendingTracking/SpendingTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingTrackingApp.swift
+//  SpendingTracking
+//
+//  Created by carl on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SpendingTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpendingTrackingTests/SpendingTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingTrackingTests.swift
+//  SpendingTrackingTests
+//
+//  Created by carl on 12/14/24.
+//
+
+import Testing
+@testable import SpendingTracking
+
+struct SpendingTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
